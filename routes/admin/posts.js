319ab5861c1e6d089b625f61791e0baa2ab6ec2b
zuber1077 <zuber1077@yahoo.com>
2018-06-03 16:22:35 +0800

    be able to delete data from db
    ..

--- routes/admin/posts.js
+++ routes/admin/posts.js
@@ -71,6 +71,12 @@ router.put('/edit/:id', (req,res)=>{
 
 });
 
+router.delete('/:id', (req,res)=>{
+    Post.remove({_id: req.params.id}).then(result=>{
+        res.redirect('/admin/posts');
+    });
+});
+
 
 
 module.exports = router;

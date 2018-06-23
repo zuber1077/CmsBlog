754fecfc735f7c50ab18e96b19bdea94a8b48257
zuber1077 <zuber1077@yahoo.com>
2018-06-23 10:48:51 +0800

    push to heroku

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports = { 
-    mongoDbUri: 'mongodb://127.0.0.1/myBlog'
+    //mongoDbUri: 'mongodb://127.0.0.1/myBlog'
+    mongoDbUri: 'mongodb://zuber:password123@ds113019.mlab.com:13019/mongoupload'
 };

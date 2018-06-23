a603dbb6b66e63d0d5f76cf85f52adbb9a55c281
zuber1077 <zuber1077@yahoo.com>
2018-06-23 11:10:03 +0800

    fix db err

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = { 
     //mongoDbUri: 'mongodb://127.0.0.1/myBlog'
-    mongoDbUri: 'mongodb://zuber:password123@ds113019.mlab.com:13019/mongoupload'
+    mongoDbUri: 'mongodb://zuber:password123@ds261470.mlab.com:61470/zuber'
 };
